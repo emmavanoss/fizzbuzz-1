6fdbca3b7cc6c97eb68c276d425d7fb1adbb6982
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-11-11 16:22:13 +0000

    first commit

---
+++ jasmine/spec/fizzBuzzSpec.js
@@ -0,0 +1,9 @@
+describe("FizzBuzz", function() {
+
+  it ('prints 1 when 1 is passed in', function() {
+    expect(fizzbuzz(1)).toEqual('1');
+  })});
+
+  // it ('prints fizz instead of 3', function() {
+  //   expect(fizzbuzz(3)).toEqual('fizz');
+  // })});
